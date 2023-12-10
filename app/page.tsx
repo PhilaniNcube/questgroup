afc53a35850e37a183f3b78f69982d1b2eb0f591
Philani Ncube <ncbphi001@gmail.com>
2023-12-10 21:32:18 +0200

    Refactor page layout and add images to utilities description

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Image from 'next/image'
+
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex justify-center space-x-5">
-     <Link href="/waterquest">Waterquest</Link>
-     <Link href="/sunquest">Sunquest</Link>
+    <main className="grid grid-cols-1 md:grid-cols-2">
+      <div className="flex min-h-screen justify-center items-center bg-blue-600 text-white text-center text-3xl">
+        <Link href="/waterquest">Waterquest</Link>
+      </div>
+      <div className="flex min-h-screen justify-center items-center bg-teal-600 text-white text-center text-3xl">
+        <Link href="/sunquest">Sunquest</Link>
+      </div>
     </main>
-  )
+  );
 }
