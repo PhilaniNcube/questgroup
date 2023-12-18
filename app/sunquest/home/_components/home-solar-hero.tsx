8ecd7354b00f75194b31f1a8b93dc9633302f227
Philani Ncube <ncbphi001@gmail.com>
2023-12-18 16:21:38 +0200

    Update home-solar-hero component styling

--- app/sunquest/home/_components/home-solar-hero.tsx
+++ app/sunquest/home/_components/home-solar-hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const HomeSolarHero = () => {
   return (
-    <div className="bg-teal-700 grid grid-cols-1 lg:grid-cols-2 min-h-[40vh] relative">
+    <div className="bg-teal-700 grid grid-cols-1 lg:grid-cols-2 min-h-[40vh] max-h-[45vh] relative">
       <div className="absolute inset-0 bg-teal-700/70 lg:bg-teal-700/0">
         <div className="container h-full grid grid-cols-1 lg:grid-cols-2">
           <div className="flex flex-col justify-center h-full pr-10">
@@ -19,13 +19,13 @@ const HomeSolarHero = () => {
         </div>
       </div>
       <div className=""></div>
-      <div className="w-full h-[40h]">
+      <div className="w-full h-full overflow-clip flex">
         <Image
           src="/images/solar-home.jpg"
           width={1920}
           height={2880}
           alt="Solar Home"
-          className="w-full aspect-video object-cover"
+          className="w-full h-[40vh] object-cover"
         />
       </div>
     </div>
