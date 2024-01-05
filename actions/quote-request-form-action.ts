cf95a4329424237b25deaf4e14a051543d66f6df
Philani Ncube <ncbphi001@gmail.com>
2024-01-05 12:59:01 +0200

    Add email rendering using @react-email/components

--- actions/quote-request-form-action.ts
+++ actions/quote-request-form-action.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import QuoteRequestEmail from '@/email/quote-form-email';
+import { renderAsync } from '@react-email/components';
 import { redirect } from 'next/navigation';
 import React from 'react';
 import { Resend } from 'resend';
@@ -18,22 +19,31 @@ const quoteFormAction = async (formData:FormData) => {
   const message = formData.get('message') as string
 
 
-
+  const html = await renderAsync(QuoteRequestEmail({
+    first_name: first_name,
+    last_name: last_name,
+    service: service,
+    phone: phone,
+    email: email,
+    message: message,
+  }) as React.ReactElement)
 
   const sending = await resend.emails.send({
     from: `Questgroup <darren@questgroup.co.za>`,
     to: ["darren@questgroup.co.za", email],
     subject: "New Quote Request",
-    text: `You have a new quote request from ${first_name} ${first_name} ${phone} - ${email}. Details: ${message}`,
-    react: React.createElement(QuoteRequestEmail, {
-      first_name: first_name,
-      last_name: last_name,
-      service: service,
-      phone: phone,
-      email: email,
-      message: message,
+    reply_to: email,
+    html: html,
+    // text: `You have a new quote request from ${first_name} ${first_name} ${phone} - ${email}. Details: ${message}`,
+    // react: React.createElement(QuoteRequestEmail, {
+    //   first_name: first_name,
+    //   last_name: last_name,
+    //   service: service,
+    //   phone: phone,
+    //   email: email,
+    //   message: message,
 
-    })
+    // })
 
   })
 
