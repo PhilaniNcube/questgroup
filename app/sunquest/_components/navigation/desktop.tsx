303c6f94f1235a69fdf50f5465a3d644d4bd4854
Philani Ncube <ncbphi001@gmail.com>
2024-01-12 09:20:03 +0200

    Refactor desktop navigation styling

--- app/sunquest/_components/navigation/desktop.tsx
+++ app/sunquest/_components/navigation/desktop.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 const DesktopNavigation = () => {
   return (
-    <header className="bg-black shadow-lg hidden lg:block">
+    <header className="shadow-lg hidden lg:block">
       <div className="container flex justify-between text-white py-3">
         <div className="flex items-center">
           <Link href="/" className="text-lg font-medium">
             <Image src="/images/sunquest-logo.png" alt="Sunquest" width={4500} height={947} className="w-48 object-cover" />
           </Link>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="flex text-black items-center space-x-4">
           <Link href="/sunquest/home">Home Solar</Link>
           <Link href="/sunquest//business">Business Solar</Link>
           {/* <Link href="/quote">
