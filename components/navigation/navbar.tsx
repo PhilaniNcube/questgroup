94a0b7c6afcfb1d825137594078893645f191c73
Philani Ncube <ncbphi001@gmail.com>
2024-01-12 09:10:29 +0200

    Update navbar logo sizes

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -11,21 +11,21 @@ const Navbar = () => {
             alt="Questgroup"
             width={1275}
             height={249}
-            className="w-44 object-cover"
+            className="w-32 md:w-44 object-cover"
           />
           <p className="sr-only">Questgroup</p>
         </Link>
-        <nav className="flex items-center space-x-4">
-          <Link href="/waterquest" className=" px-4 py-2 font-bold">
+        <nav className="flex items-center">
+          <Link href="/waterquest" className=" px-2 py-2 font-bold">
             <Image
               src="/images/logo.png"
               alt="Waterquest"
               width={4500}
               height={947}
-              className="w-32 lg:w-44 object-cover"
+              className="w-40 lg:w-44 object-cover"
             />
           </Link>
-          <Link href="/sunquest" className="px-4 py-2 font-bold">
+          <Link href="/sunquest" className="px-2 py-2 font-bold">
             <Image
               src="/images/sunquest-logo.png"
               alt="Waterquest"
