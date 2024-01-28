613b47791036bc0fd2e58810e603bf906f51d1ef
Philani Ncube <ncbphi001@gmail.com>
2024-01-28 14:19:56 +0200

    Update titles and colors in reasons.tsx

--- app/(home)/reasons.tsx
+++ app/(home)/reasons.tsx
@@ -1,7 +1,7 @@
 
 const cards = [
   {
-    title: "Solar Power Installations powered by SIGENERGY",
+    title: "SIGENERGY Solar Power Installations",
     description:
       "Upgrade to smart solar power with our professional SIGENERGY installations. They are reliable, scalable, and can result in significant energy cost savings for your home or business.",
   },
@@ -11,7 +11,7 @@ const cards = [
       "Enjoy high energy yield with our solar power installations. SIGENERGY products are designed for optimum power performance and maintain high-efficiency levels even in low light environments.",
   },
   {
-    title: "Solar Water Pumps with Lorentz Technology",
+    title: "Lorentz Solar Water Pumps",
     description:
       "Experience the dependability of sustainable water supply systems with our Lorentz solar water pumps. They are perfectly designed for various applications, including irrigation, livestock, or supplying potable water for your residential complex.",
   },
@@ -35,7 +35,7 @@ const cards = [
 const Reasons = () => {
   return (
     <section className="py-5">
-      <div className="container rounded-lg shadow-lg bg-fuchsia-950 p-8 text-white">
+      <div className="container rounded-lg shadow-lg bg-orange-700 p-8 text-white">
         <h3 className="text-center text-2xl font-semibold">
           Innovative Renewable Energy Solutions for a Sustainable Future
         </h3>
@@ -45,8 +45,8 @@ const Reasons = () => {
               key={index}
               className="flex flex-col p-4 rounded-lg"
             >
-              <h4 className="text-xl font-semibold">{card.title}</h4>
-              <p className="text-sm mt-2 text-fuchsia-200 leading-6">{card.description}</p>
+              <h4 className="text-lg font-semibold">{card.title}</h4>
+              <p className="text-sm mt-2 text-orange-50 leading-6">{card.description}</p>
             </div>
           ))}
         </div>
